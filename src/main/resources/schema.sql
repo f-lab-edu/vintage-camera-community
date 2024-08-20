484829b3e0072fe6f72024336b6f5100c520e9cd
0Zone <00zero0zone00@gmail.com>
2024-08-20 21:36:58 +0900

    #24 1) 동일한 스키마에 동일한 이름의 인덱스 두 번 생성되는 부분 수정.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,7 @@
  -- 모임 게시글 검색 기능에 대한 부분 패턴 일치에 대한 인덱싱 처리
- CREATE INDEX IF NOT EXISTS idx_meeting_fulltext ON meeting USING GIN (to_tsvector('simple', title || ' ' || description));
+ DO $$
+ BEGIN
+   IF NOT EXISTS (SELECT 1 FROM pg_class WHERE relname = 'idx_meeting_fulltext') THEN
+ CREATE INDEX idx_meeting_fulltext ON meeting USING GIN (to_tsvector('simple', title || ' ' || description));
+ END IF;
+ END $$;
