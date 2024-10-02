dfb7240777c2941a8872d78ddd08bf97d190a440
0Zone <00zero0zone00@gmail.com>
2024-10-02 15:31:47 +0900

    #26 1) 불필요 소스 제거

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,5 +6,5 @@
  END IF;
  END $$;
 
- # -- 모임 게시글 검색 기능에 대한 부분 패턴 일치에 대한 인덱싱 처리
+-- 모임 게시글 검색 기능에 대한 부분 패턴 일치에 대한 인덱싱 처리
 -- CREATE INDEX IF NOT EXISTS idx_meeting_fulltext ON meeting USING GIN (to_tsvector('simple', title || ' ' || description));
